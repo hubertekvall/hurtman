48261b0d589b8053748601b9df20f5efec405d99
Hubert Ekvall <hubertekvall@gmail.com>
2026-03-03 03:03:21 +0100

    Update AltitudeControl.cs

--- Actors/Components/Physics/Movement/AltitudeControl.cs
+++ Actors/Components/Physics/Movement/AltitudeControl.cs
@@ -8,10 +8,16 @@ public partial class AltitudeControl : Node, IActorComponent, IPhysicsHandler
     
     
     [Export]
-    public 
+    public float AltitudeTarget { get; set; }
+
+    [Export]
+    public float AltitudeStrength { get; set; }
     
-    
-    
+    [Export]
+    public float AltitudeDamping { get; set; }
+
+
+
     public void Setup()
     {
         PhysicsComponent = Actor.GetComponent<IPhysicsComponent3D>();
@@ -19,6 +25,16 @@ public partial class AltitudeControl : Node, IActorComponent, IPhysicsHandler
 
     public void PhysicsTick(float delta)
     {
+        if(PhysicsComponent == null) return;
         
+        var velocity = PhysicsComponent.Velocity;
+        var position = PhysicsComponent.GlobalTransform.Origin;
+        
+        var offset = position.Y - AltitudeTarget;
+
+        var springForce = (AltitudeStrength * offset) - (AltitudeDamping * velocity.Y);
+
+        PhysicsComponent.ApplyForce(springForce * Vector3.Up);
+
     }
 }
